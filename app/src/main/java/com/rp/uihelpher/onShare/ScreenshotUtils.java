d6464a5ff4639d0d55959aa24a30da057e7082c2
rpmobiledevelopment <rajapandiramalingam.r@gmail.com>
2025-09-04 18:07:39 +0530

    add multiple UI

--- app/src/main/java/com/rp/uihelpher/onShare/ScreenshotUtils.java
+++ app/src/main/java/com/rp/uihelpher/onShare/ScreenshotUtils.java
@@ -4,18 +4,17 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Environment;
 
-import com.mm.uihelper.R;
+import com.rp.uihelpher.log.IsLog;
 
 import java.io.File;
 import java.io.FileOutputStream;
 
-import log.IsLog;
 
 public class ScreenshotUtils {
 
     public static File getMainDirectoryName(Context context) {
         File mainDir = new File(
-                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), context.getResources().getString(R.string.app_name));
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), context.getResources().getString(com.rp.uihelpher.R.string.app_name));
         //If File is not present create directory
         if (!mainDir.exists()) {
             if (mainDir.mkdir())
