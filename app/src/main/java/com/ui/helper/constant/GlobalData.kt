37eb4713cfe7579376db9f770510524faed368c5
rpmobiledevelopment <rajapandiramalingam.r@gmail.com>
2026-04-15 18:21:15 +0530

    ui helper

--- app/src/main/java/com/ui/helper/constant/GlobalData.kt
+++ app/src/main/java/com/ui/helper/constant/GlobalData.kt
@@ -4,15 +4,16 @@ interface GlobalData {
     companion object {
         const val TAG_SELECT_PROJ_NAME: String = "TAG_SELECT_PROJ_NAME"
         const val TAG_SELECTED_LANGUAGE: String = "TAG_SELECTED_LANGUAGE"
-        const val TAG_BASE_URL: String = "TAG_BASE_URL"
-        const val TAG_BEAR_TOKEN: String = "TAG_BEAR_TOKEN"
-        const val TAG_SELECT_LANGUAGE: String = "TAG_SELECT_LANGUAGE"
         const val TAG_VIDEO_PATH: String = "TAG_VIDEO_PATH"
         const val TAG_VIDEO_PATH_NAME: String = "TAG_VIDEO_PATH_NAME"
         const val SHARED_PREF: String = "SHARED_PREF"
         const val TAG_FCM_KEY: String = "TAG_FCM_KEY"
         const val TAG_CURR_LAT: String = "TAG_CURR_LAT"
         const val TAG_CURR_LNG: String = "TAG_CURR_LNG"
+
+        var TAG_BASE_URL: String = "TAG_BASE_URL"
+        var TAG_BEAR_TOKEN: String = "TAG_BEAR_TOKEN"
+        var TAG_SELECT_LANGUAGE: String = "TAG_SELECT_LANGUAGE"
 
         var TAG_API_PIN: Boolean = false
         var TAG_API_PIN_BASE_URL: String = "TAG_API_PIN_BASE_URL"
